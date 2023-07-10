83b5c09e7d15d8fe780bd7fc81126db9ebfaac94
humanist96 <humanist96@gmail.com>
2023-07-10 16:10:44 +0900

    Update app.py
    prompt 개선

--- app.py
+++ app.py
@@ -3,17 +3,44 @@ import openai
 
 openai.api_key = st.secrets["api_key"]
 
-st.title("ChatGPT Plus DALL-E")
+st.title("Kevin의 AI 이미지 생성기")
 
 with st.form("form"):
     user_input = st.text_input("Prompt")
     size = st.selectbox("Size", ["1024x1024", "512x512", "256x256"])
     submit = st.form_submit_button("Submit")
 
+
 if submit and user_input:
     gpt_prompt = [{
         "role": "system",
-        "content": "Imagine the detail appeareance of the input. Response it shortly around 20 words"
+        "content": (
+            "You are now a Dall-E prompt engineer."
+            "Your job is to configure the prompts to generate the best image in Dall-E for what I ask."
+
+            "If I ask you a question and the following conditions are present, please continue with the question to build a good prompt."
+            "1. I didn't clearly recognize my question.\n"
+            "2. I'm talking about an image that Dall-E can't generate.\n"
+            "3. I've used words that are too abstract or imprecise.\n"
+            "4. Until I entered the information I needed and said, 'Make me a prompt like this'. \n"
+
+            "I want to know how well you understood my question and faithfully reflected it in your prompt."
+            "So at the end of each question I ask, give me a confidence score from 0 to 100."
+
+            "The elements of the prompt you need to create must meet all of the following conditions"
+            "1. Make every effort to satisfy my question.\n"
+            "2. Contextual understanding, background, and creative thinking about the question.\n"
+            "3. How and what you use in your answer\n"
+            "4. Includes what style you should use when creating your answer, etc.\n"
+            "5. Generating ideal prompts for every topic you can imagine.\n"
+
+            "When you write out the prompt, make sure you fulfill all of the following conditions"
+            "1. Use literal, clear language\n"
+            "2. Act like an expert on the topic.\n"
+            "3. Act like a specific artist or combination of artists.\n"
+            "4. Keep it to one sentence whenever possible.\n"
+            "5. Output the prompt in English.\n"
+        )
     }]
 
     gpt_prompt.append({
